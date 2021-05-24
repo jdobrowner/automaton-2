672dbb1e9b37d9f8788e9b3341ccf1d6c02853bc
Jason Dobrowner <jdobrowner@gmail.com>
2021-05-23 23:26:40 -0700

    initial state button

---
+++ src/components/initialStateButton/InitialStateButton.svelte
@@ -0,0 +1,17 @@
+<script lang="typescript">
+    import { InitialStateOptions } from 'src/constants';
+import InitialStatePaths from './initialStatePaths';
+    export let pattern: InitialStateOptions;
+    export let title: string;
+    export let colors: string[] = [];
+
+    let patternPaths: string[] = InitialStatePaths[pattern];
+</script>
+
+<div {title} onClick={() => {}} style="background-color={colors[0]}">
+    <svg viewBox="0 0 35 35">
+      {#each patternPaths as pPath}
+        <path d={pPath} stroke={colors[3]} stroke-width="2" fill="none" />
+      {/each}
+  </svg>
+</div>
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export enum InitialStateOptions {
+  LITTLE_TRIANGLE,
+  NESTED_TRIANGLE,
+  HEXAGON,
+  BORDER,
+  TRIFORCE,
+}
