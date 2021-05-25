3f1c37e1fa3c7bd409106a2a219b99693282c8aa
Jason Dobrowner <jdobrowner@gmail.com>
2021-05-24 23:07:38 -0700

    constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,33 @@ export enum InitialStateOptions {
   BORDER,
   TRIFORCE,
 }
+
+export enum ColorThemeNames {
+  PURPLE,
+  GREEN,
+  BRIGHTS,
+  RED,
+  OCEAN,
+  SOUTHWEST,
+}
+
+export enum RandomnessStates {
+  ON,
+  OFF,
+}
+
+export enum Rulesets {
+  EXPANDER,
+  HARMONY,
+  MANGLER,
+  HORIZONS,
+  BIRDS,
+  BILLOW,
+  SWIRLS,
+}
+
+export enum Speeds {
+  SLOW,
+  MEDIUM,
+  FAST,
+}
