27b8b8249bb054ea00f886087ac246afa386c99d
Jason Dobrowner <jdobrowner@gmail.com>
2021-05-24 23:08:03 -0700

    settings stores

---
+++ src/stores/SettingsStores.ts
@@ -0,0 +1,19 @@
+import {
+  ColorThemeNames,
+  InitialStateOptions,
+  RandomnessStates,
+  Rulesets,
+  Speeds,
+} from "src/constants";
+import Colors from "../colors";
+import { writable } from "svelte/store";
+
+export const colors = writable(Colors[ColorThemeNames.SOUTHWEST]);
+
+export const initialState = writable(InitialStateOptions.LITTLE_TRIANGLE);
+
+export const randomness = writable(RandomnessStates.OFF);
+
+export const ruleset = writable(Rulesets.EXPANDER);
+
+export const speed = writable(Speeds.MEDIUM);
